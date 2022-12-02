2c0a4bbb8fcd1b2b13fe8b96499f3462c020fea2
SilvaCastillo <60104481+SilvaCastillo@users.noreply.github.com>
2022-12-02 18:47:21 +0100

    Show zones of one city
    Get geoJSON from the database to show the different zones that exist in that city. Show different zones with different colors.

--- src/lib/map.js
+++ src/lib/map.js
@@ -1,8 +1,17 @@
-export default function map() {
+
+const api_url = 'http://localhost:3000/v1/cities/6384bb98897c01a69121c994';
+
+
+export default async function map() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const {Geojson } = data;
+
+
     let L = require("leaflet");
 
     let map = L.map("map", {
-        scrollWheelZoom: false,
+        scrollWheelZoom: true,
     });
     
     map.setView([47.7, 13.35], 7);
@@ -12,6 +21,31 @@ export default function map() {
         attribution:
             '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     });
-    
+
+
     tile.addTo(map);
+
+    L.geoJSON(Geojson.Limit,{
+        style:{
+            color: 'black',
+            fillOpacity: 0
+        }
+    }).addTo(map);
+
+    L.geoJSON(Geojson.Charging,{
+        style:{
+            color: 'blue',
+            fillOpacity: 0.3
+        }
+    }).addTo(map);
+
+
+    L.geoJSON(Geojson.Parking,{
+        style:{
+            color: 'green',
+            fillOpacity: 0.3
+        }
+    }).addTo(map);
+
+
 }
