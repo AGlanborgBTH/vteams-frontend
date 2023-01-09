6466f68e94a5d16db684a1dd74f137af7a34c0e6
kalinchukkristina <89469456+kalinchukkristina@users.noreply.github.com>
2023-01-09 14:25:08 +0100

    Update updateWallet.js

--- src/requests/updateWallet.js
+++ src/requests/updateWallet.js
@@ -2,9 +2,8 @@ import { useCookies } from "vue3-cookies";
 const { cookies } = useCookies();
 import axios from "axios";
 
-let user = cookies.get("user");
-
 export default async function updateWallet(value) {
+  let user = cookies.get("user");
   try {
     // Send a patch request to update the wallet value
     await axios.patch(`http://localhost:3000/v1/users/${user.id}`, {
